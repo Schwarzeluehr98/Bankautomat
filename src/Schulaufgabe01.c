b55ac9806a5f52f61eb7a6b9b88d6084b514d43d
Lars Schwarzeluehr <schwarzeluehr.lars@t-online.de>
2016-10-06 15:49:39 +0200

    Funktionen eingebaut

--- src/Schulaufgabe01.c
+++ src/Schulaufgabe01.c
@@ -10,26 +10,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void printmenu(void){
+void printMenu(void) {
 	printf("**************************************************  \n");
 	printf("Der Geldautomat der Bank 007  \n");
 	printf("\n**************************************************  \n");
 	printf("Bitte schieben Sie Ihre Karte ein (KtoNr eingeben)  \n");
 }
 
-void printPIN(void){
+void printPINabbruch(void) {
 	printf("PIN drei mal falsch eingegeben\n");
 	printf("Vorgang wird abgebrochen\n");
 }
 
-void printAuszahlung(void){
+void printAuszahlung(void) {
 	printf("\n");
 	printf("Der oben angezeigte Betrag wird Ihnen ausgezahlt\n");
 	printf("Auf Wiedersehen\n");
 }
 
-int pinEingabe(void){
+int pinEinlesen(void) {
 	int _pin;
 	printf("Geben Sie Ihren PIN ein\n");
 	scanf("%d", &_pin);
@@ -38,67 +39,70 @@ int pinEingabe(void){
 	return _pin;
 }
 
+bool gueltigerPIN(void) {
+	int _temp;
+	for (_temp = 0; _temp < 3; _temp++) {
+		if (pinEinlesen() == 111) {
+			printf("Ihr PIN ist gültig\n");
+			return true;
+		} else {
+			printf("Ungültiger PIN\n");
+		}
+	}
+	printPINabbruch();
+	return false;
+}
 
-int main() {
-	// lokale Variablen definieren
-	int ktoNr;
-	int temp = 0;
-	int Vorgang;
+void geldscheinAusgabe(void) {
 	int n, i;
 	int scheine[7] = { 500, 200, 100, 50, 20, 10, 5 };
+
+	scanf("%d", &n);
+	for (i = 0; i < 7; i++) {
+		if (n / scheine[i] != 0)
+			printf("%d x %d\t", n / scheine[i], scheine[i]);
+		n %= scheine[i];
+	}
+}
+
+int auswahl(void) {
+	int Vorgang;
+	printf("Vorgang 1: abbrechen oder 2: fortfahren\n");
+	scanf("%d", &Vorgang);
+	return Vorgang;
+}
+
+int main() {
+
+	// lokale Variablen definieren
+	int ktoNr;
+	int Vorgang;
 
 	// Endlosschleife
 	while (1) {
 		// das Menü
-		printmenu();
+		printMenu();
 		// Einlesen der Kontonummer
 		scanf("%d", &ktoNr);
-
 		// Überprüfung der Kontonummer (=1234)
 		if (ktoNr == 1234) {
-			temp = 0;
-
-			// Eine Möglichkeit der PIN-Abfrage
-			// hier Lösung mit for()-Schleife
-			for (temp = 0; temp < 3; temp++) {
-				if (pinEingabe() == 111) {
-					printf("Ihr PIN ist gültig\n");
-					// Scheife beenden
-					break;
-				} else {
-					printf("PIN falsch eingegeben\n");
-				}
+			if (gueltigerPIN() == false)
+				continue;
+			//Auswahl des weiteren Vorgangs
+			Vorgang=auswahl();
+			// Bei 1 = Abbruch
+			if (Vorgang == 1) {
+				printf("Der Vorgang wird nun abgebrochen\n");
+				continue;
+			}	//Bei 2 = Betrag angeben
+			if (Vorgang == 2) {
+				printf("Geben Sie den Betrag an: \n");
 			}
-
-			//Beim dreimaligem PIN Fehlversuch = Abbruch
-			if (temp > 3) {
-				printPIN();
-			} else {
-				printf("PIN ist OK\n");
-				//Auswahl wie der Vorgang weiter gehen soll
-				printf("Vorgang 1: abbrechen oder 2: fortfahren\n");
-				scanf("%d", &Vorgang);
-				// Bei 1 = Abbruch
-				if (Vorgang == 1) {
-					printf("Der Vorgang wird nun abgebrochen\n");
-				}	//Bei 2 = Betrag angeben
-				if (Vorgang == 2) {
-					printf("Geben Sie den Betrag an: \n");
-					scanf("%d", &n);
-					//Scheinauswahl
-					for (i = 0; i < 7; i++) {
-						if (n / scheine[i] != 0)
-							printf("%d x %d\t", n / scheine[i], scheine[i]);
-						n %= scheine[i];
-					}	//Der Betrag wird ausgezahlt
-						printAuszahlung();
-				}
-			}
-
+			// Geldscheinausgabe
+			geldscheinAusgabe();
+			printAuszahlung();
 		}
-
 	}   // end of while(1)
-
 	return 0;
 }	// ENDE DES HAUPTPROGRAMMS
 
