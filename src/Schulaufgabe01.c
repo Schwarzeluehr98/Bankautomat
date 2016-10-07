6b120e11117665e011deb38a1c33cc3b2d2c746e
Lars Schwarzeluehr <schwarzeluehr.lars@t-online.de>
2016-10-07 15:40:01 +0200

    Betrag Auszahlung optimiert

--- src/Schulaufgabe01.c
+++ src/Schulaufgabe01.c
@@ -72,11 +72,60 @@ int auswahl(void) {
 	return Vorgang;
 }
 
+enum x {
+	Abbruch = 1, Betrageingabe = 2
+};
+
+enum z {
+	Geldscheinwahl = 1, Betrag
+};
+
+int wahl(void) {
+	int Moeglichkeit;
+	scanf("%d", &Moeglichkeit);
+	return Moeglichkeit;
+}
+
+int geldscheinWahl(void) {
+	int Schein;
+	printf("Geben sie den Schein an\n");
+	scanf("%d", &Schein);
+
+	switch (Schein) {
+	case 1:
+		printf("Sie haben den 500€ Schein gewählt\n");
+		break;
+	case 2:
+		printf("Sie haben den 200€ Schein gewählt\n");
+		break;
+	case 3:
+		printf("Sie haben den 100€ Schein gewählt\n");
+		break;
+	case 4:
+		printf("Sie haben den 50€ Schein gewählt\n");
+		break;
+	case 5:
+		printf("Sie haben den 20€ Schein gewählt\n");
+		break;
+	case 6:
+		printf("Sie haben den 10€ Schein gewählt\n");
+		break;
+	case 7:
+		printf("Sie haben den 5€ Schein gewählt\n");
+		break;
+	default:
+		printf(
+				"Bei erneutem Versuch bitte eine der möglichen Angaben auswählen\n");
+	}
+	return 0;
+}
+
 int main() {
 
 	// lokale Variablen definieren
 	int ktoNr;
-	int Vorgang;
+	enum x Vorgang;
+	enum z Moeglichkeit;
 
 	// Endlosschleife
 	while (1) {
@@ -89,20 +138,39 @@ int main() {
 			if (gueltigerPIN() == false)
 				continue;
 			//Auswahl des weiteren Vorgangs
-			Vorgang=auswahl();
+			Vorgang = auswahl();
 			// Bei 1 = Abbruch
-			if (Vorgang == 1) {
+			if (Vorgang == Abbruch) {
 				printf("Der Vorgang wird nun abgebrochen\n");
 				continue;
-			}	//Bei 2 = Betrag angeben
-			if (Vorgang == 2) {
-				printf("Geben Sie den Betrag an: \n");
 			}
-			// Geldscheinausgabe
-			geldscheinAusgabe();
+			//Bei 2 = Betrag angeben
+			if (Vorgang == Betrageingabe) {
+				printf("Wahl der weiteren Vorgehensweise:\n"
+						"1 = Geldscheinwahl\n"
+						"2 = Betrag auswählen \n");
+			}
+			// Geldscheinauswahl
+			Moeglichkeit = wahl();
+			if (Moeglichkeit == Geldscheinwahl) {
+				printf("Wahl der Geldscheine\n"
+						"1 = 500€\n"
+						"2 = 200€\n"
+						"3 = 100€\n"
+						"4 = 50€\n"
+						"5 = 20€\n"
+						"6 = 10€\n"
+						"7 = 5€\n");
+				geldscheinWahl();
+			}
+			//Betragangabe
+			if (Moeglichkeit == Betrag) {
+				printf("Geben Sie nun den gewünschten Betrag ein\n");
+				geldscheinAusgabe();
+			}
+			//Erfolgreiche Auszahlung
 			printAuszahlung();
-		}
+		} // end of if
 	}   // end of while(1)
 	return 0;
 }	// ENDE DES HAUPTPROGRAMMS
-
